b8b329b0f5140953d8f770c3fa38cc0a96bf885d
Farrel Satya Putra <106051750+farrelsp@users.noreply.github.com>
2024-11-14 13:11:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,4 +96,31 @@ df_prediction_proba.rename(columns={0: "Adelie", 1: "Gentoo", 2: "Chinstrap"})
 
 # Display prediction
 st.subheader("Predicted Species")
-st.dataframe(df_prediction_proba)
+st.dataframe(df_prediction_proba,
+             column_config={
+               'Adelie': st.column_config.ProgressColumn(
+                 'Adelie',
+                 format='%f',
+                 width='medium',
+                 min_value=0,
+                 max_value=1
+               ),
+               'Chinstrap': st.column_config.ProgressColumn(
+                 'Chinstrap',
+                 format='%f',
+                 width='medium',
+                 min_value=0,
+                 max_value=1
+               ),
+               'Gentoo': st.column_config.ProgressColumn(
+                 'Gentoo',
+                 format='%f',
+                 width='medium',
+                 min_value=0,
+                 max_value=1
+               ),
+             }, hide_index=True)
+
+penguin_species = np.array(['Adelie', 'Gentoo', 'Chinstrap'])
+st.write(prediction)
+st.success(str(penguin_species[prediction][0]))
