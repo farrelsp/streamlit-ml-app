23af57db0411df44dfc829c929766a6f4ec16228
Farrel Satya Putra <106051750+farrelsp@users.noreply.github.com>
2024-11-14 11:44:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🐧 Penguin Predictor App')
 
 st.info('This app builds machine learning model to predict penguin species!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
